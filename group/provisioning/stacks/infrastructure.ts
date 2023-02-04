7ef05d643179f3d51166e89293cffc5cc60e4bfe
Christophe Tiraoui <krux@krux.fr>
2023-02-04 06:40:48 +0100

    tpl

--- group/provisioning/stacks/infrastructure.ts
+++ group/provisioning/stacks/infrastructure.ts
@@ -5,12 +5,14 @@ import <%= component %>Infra from "@<%= partition %>/<%= group %>-<%= component 
 import { config, formats } from "@tuxounet-k2/builder";
 import * as cdk from "aws-cdk-lib";
  
-const currentConfig: config.Config = config.loadConfig()
+ 
 const app = new cdk.App();
 
 <% for(const component of components) { %>
-const <%= component %>Config: config.Config = config.loadConfig({        group : `<%= group %>`, component: `<%= component %>`,     });
+  
+const <%= component %>Config: config.Config = config.loadConfig({ group : `<%= group %>`, component: `<%= component %>` });
 new <%= component %>Infra(app, formats.formatRessourceId(`<%= group %>`, `<%= component %>`, "infra"), <%= component %>Config);
+
 <% } %>
 
 export default app;
