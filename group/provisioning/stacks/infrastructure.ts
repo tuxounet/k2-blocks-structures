3c3f6e81c4f75b7c4fdd81e7498c937899c698de
Christophe Tiraoui <krux@krux.fr>
2023-02-04 04:28:56 +0100

    passing config to stack

--- group/provisioning/stacks/infrastructure.ts
+++ group/provisioning/stacks/infrastructure.ts
@@ -2,22 +2,14 @@ import "source-map-support/register";
 <% for(const component of components) { %>
 import <%= component %>Infra from "@<%= partition %>/<%= group %>-<%= component %>-infra";
 <% } %>
-import { config, formats, types } from "@tuxounet-k2/builder";
+import { config, formats } from "@tuxounet-k2/builder";
 import * as cdk from "aws-cdk-lib";
  
-const currentConfig: types.IStackProps = config.loadConfig()
-
+const currentConfig: config.Config = config.loadConfig()
 const app = new cdk.App();
 
 <% for(const component of components) { %>
-new <%= component %>Infra(app, formats.formatRessourceId(`<%= group %>`, `<%= component %>`, "infra"), {
-  "<%= group %>-<%= component %>": currentConfig["<%= group %>-<%= component %>"] as any,
-  ...currentConfig,
-  group: "<%= group %>", 
-  component: "<%= component %>",
-});
-
+new <%= component %>Infra(app, formats.formatRessourceId(`<%= group %>`, `<%= component %>`, "infra"), currentConfig);
 <% } %>
 
- 
 export default app;
